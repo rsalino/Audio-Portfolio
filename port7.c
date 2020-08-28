7463f8335d2e05596052258cbfc7607105b73d66
rsalino <55030115+rsalino@users.noreply.github.com>
2020-08-28 10:22:56 -0700

    Update port7.c

--- port7.c
+++ port7.c
@@ -3,6 +3,7 @@
 // IF CAPTURING SYSTEM AUDIO ON MAC OS, RECOMMEND USING SOUNDFLOWER:
 // https://github.com/mattingalls/Soundflower
 // Created by Rob Salino on 12/7/19
+// Some portions are from helloring.c in The Audio Programming Book
 // by Richard Boulanger and Victor Lazzarini, ISBN: 978-0-262-01446-5
 //
 // run with: cc -o portrecord Port_Record.c -lsndfile -lportaudio
